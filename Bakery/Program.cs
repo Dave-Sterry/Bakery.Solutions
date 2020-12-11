842303e18852d0d0741c3e9b509cfe54c9ddc40c
David Sterry <sterry.david@gmail.com>
2020-12-11 11:28:46 -0800

    add branching too console app

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -10,11 +10,25 @@ public class Customer
     Console.WriteLine("Welcome to Pierre's Bakery!");
     Console.WriteLine("---------------------------");
     Console.WriteLine("We have bread and pastries");
-    Console.WriteLine("Would you like to order some bread ['Y' for yes, 'Enter' for no]");
+    Console.WriteLine("What would you like to order? [Enter 'bread' for bread, 'pastries' for pastries 'both' if you want to order both]");
     string answer = Console.ReadLine();
-    if (answer == "Y" || answer "y")
-   
-    
+    if (answer == "bread")
+    {
+      Console.WriteLine("How many loaves would you like?");
+      string amount = Console.ReadLine();
+    }
+    else if (answer == "pastries")
+    {
+      Console.WriteLine("How many pastries would you like?");
+      string amount = Console.ReadLine();
+    }
+    else if (answer == "both") ;
+    {
+      Console.WriteLine("How many loaves would you like?");
+      string breads = Console.ReadLine();
+      Console.WriteLine("How many pastries would you like?");
+      string pastries = Console.ReadLine();
+    }
 
   }
 }
