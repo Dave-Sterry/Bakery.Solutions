ef800cfd39b1b788d92a5876ba8ef9bf30f278e7
David Sterry <sterry.david@gmail.com>
2020-12-11 10:23:01 -0800

    purposefully pass pastry price test

--- Bakery/Models/Pastry.cs
+++ Bakery/Models/Pastry.cs
@@ -5,15 +5,30 @@ namespace Bakery.Models
 {
   public class Pastry
   {
-
+    public int PastryOrder { get; set; }
+    public int PastryPrice { get; set; }
     public Pastry(int order)
     {
-
+      PastryOrder = order;
+      PastryPrice = order * 2;
     }
 
     public int OrderTotal()
     {
-      return 0;
+      if (PastryOrder % 3 == 0)
+      {
+        int price = (PastryOrder / 3) * 5;
+        return price;
+      }
+      else if (PastryOrder % 3 > 0 && PastryOrder > 3)
+      {
+        int price = ((PastryOrder / 3) * 5) + ((PastryOrder % 3) * 2);
+        return price;
+      }
+      else
+      {
+        return PastryPrice;
+      }
     }
   }
 }
