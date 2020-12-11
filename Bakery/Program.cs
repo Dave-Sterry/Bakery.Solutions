865b34c11f030cdd0c95b747247b968bda575a4b
David Sterry <sterry.david@gmail.com>
2020-12-11 15:33:09 -0800

    touch up console app grammar

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -8,14 +8,14 @@ public class Customer
   {
     Console.WriteLine("Welcome to Pierre's Bakery!");
     Console.WriteLine("---------------------------");
-    Console.WriteLine("Hello fellow gluten lover! We have bread and pastries");
+    Console.WriteLine("Hello fellow gluten lover! We have bread and pastries for sale today");
     Console.WriteLine("Today our bread is $5 or you can buy 2 and get 1 free!");
-    Console.WriteLine("Our pastries today are $2 or 3 for $5");
+    Console.WriteLine("Our pastries today are $2, or 3 for $5");
     Console.WriteLine("How many loaves of bread can I get you today?");
     int loaves = int.Parse(Console.ReadLine());
     Console.WriteLine($"{loaves} loaves eh? How many delicious pastries can I add to your order today?");
     int sweets = int.Parse(Console.ReadLine());
-    Console.WriteLine("I forgot to mention we have coffee! Its $3 a cup, unless you buy more than 10 and thens its $2 a cup");
+    Console.WriteLine("I forgot to mention we have coffee! Its $3 a cup, unless you buy more than 10cups and then its $2 a cup");
     Console.WriteLine("How many cups of coffee would you like to add to your order?");
     int coffees = int.Parse(Console.ReadLine());
 
