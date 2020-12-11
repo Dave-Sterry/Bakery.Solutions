ca168acb9dcae6cb79c584b373e6e24fb1780b51
David Sterry <sterry.david@gmail.com>
2020-12-11 11:09:03 -0800

    start console app build out

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -1,5 +1,5 @@
 using System;
-using Bakery;
+using Bakery.Models;
 
 
 public class Customer
@@ -7,6 +7,14 @@ public class Customer
 
   static void Main()
   {
+    Console.WriteLine("Welcome to Pierre's Bakery!");
+    Console.WriteLine("---------------------------");
+    Console.WriteLine("We have bread and pastries");
+    Console.WriteLine("Would you like to order some bread ['Y' for yes, 'Enter' for no]");
+    string answer = Console.ReadLine();
+    if (answer == "Y" || answer "y")
+   
+    
 
   }
 }
