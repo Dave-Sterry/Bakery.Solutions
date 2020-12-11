64b1721b526a68830db67978d1dedf650df9733f
David Sterry <sterry.david@gmail.com>
2020-12-11 13:04:45 -0800

    removed commented out unused code

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -10,6 +10,8 @@ public class Customer
     Console.WriteLine("Welcome to Pierre's Bakery!");
     Console.WriteLine("---------------------------");
     Console.WriteLine("Hello fellow gluten lover! We have bread and pastries");
+    Console.WriteLine("Today our bread is $5 or you can buy 2 and get 1 free!");
+    Console.WriteLine("Our pastries today are $2 or 3 for $5");
     Console.WriteLine("How many loaves of bread can I get you today?");
     int loaves = int.Parse(Console.ReadLine());
     Console.WriteLine($"{loaves} loaves eh? How many delicious pastries can I add to your order today?");
@@ -20,34 +22,5 @@ public class Customer
 
     Console.WriteLine($"Ok that's {loaves} loaves of bread, and {sweets} pastries");
     Console.WriteLine($"Your order total is ${loafOrder.OrderTotal() + sweetOrder.OrderTotal()}");
-
-
-
-
-
-    // string answer = Console.ReadLine();
-    // if (answer == "bread")
-    // {
-    //   Console.WriteLine("How many loaves would you like?");
-    //   int loaves = int.Parse(Console.ReadLine());
-
-    //   Bread loafOrder = new Bread(loaves);
-    //   Console.WriteLine(loafOrder.OrderTotal());
-    // }
-    // else if (answer == "pastries")
-    // {
-    //   Console.WriteLine("How many pastries would you like?");
-    //   int amount = int.Parse(Console.ReadLine());
-
-    //   Pastry sweetOrder = new Pastry(amount);
-    // }
-    // else if (answer == "both") ;
-    // {
-    //   Console.WriteLine("How many loaves would you like?");
-    //   int breads = int.Parse(Console.ReadLine());
-    //   Console.WriteLine("How many pastries would you like?");
-    //   int pastries = int.Parse(Console.ReadLine());
-    // }
-
   }
 }
