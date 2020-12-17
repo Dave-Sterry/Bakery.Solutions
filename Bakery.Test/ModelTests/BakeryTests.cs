b7a84e992a73e9c1a9474518d36b730ba4e98e0f
David Sterry <sterry.david@gmail.com>
2020-12-16 19:45:35 -0800

    add and pass constructor tests for all constructors

--- Bakery.Test/ModelTests/BakeryTests.cs
+++ Bakery.Test/ModelTests/BakeryTests.cs
@@ -7,6 +7,13 @@ namespace Bakery.Tests
   public class BreadTests
   {
     [TestMethod]
+    public void BreadConstructor_CreatesInstanceOfBread_Bread()
+    {
+        Bread newBread = new Bread(5);
+        Assert.AreEqual(typeof(Bread), newBread.GetType());
+    }
+
+    [TestMethod]
     public void OrderCost_GetsOrderDealTotal_Int()
     {
       int breadOrder = 6;
@@ -28,6 +35,13 @@ namespace Bakery.Tests
     public class PastryTests
     {
       [TestMethod]
+      public void PastyConstructor_CreatesInstanceOfPastry_Pasty()
+      {
+        Pastry newPastry = new Pastry(4);
+        Assert.AreEqual(typeof(Pastry), newPastry.GetType());
+      }
+
+      [TestMethod]
       public void OrderCost_GetsPastryTotal_Int()
       {
         int pastryOrder = 1;
@@ -35,22 +49,28 @@ namespace Bakery.Tests
         int result  = newOrder.OrderTotal();
         Assert.AreEqual( 2, result);
         }
+
+      [TestMethod]
+      public void OrderCost_GetsPastryDealTotal_Int()
+      {
+        int pastryOrder = 3;
+        Pastry newOrder = new Pastry(pastryOrder);
+        int result  = newOrder.OrderTotal();
+        Assert.AreEqual( 5, result);
+      }
     }
 
-    [TestMethod]
-    public void OrderCost_GetsPastryDealTotal_Int()
-    {
-      int pastryOrder = 3;
-      Pastry newOrder = new Pastry(pastryOrder);
-      int result  = newOrder.OrderTotal();
-      Assert.AreEqual( 5, result);
-    }
-
-    
 
     [TestClass]
     public class CoffeeTests
     {
+      [TestMethod]
+      public void CoffeeConstructor_CreatesInstanceOfCoffee_Coffe()
+      {
+        Coffee newCoffee = new Coffee(3);
+        Assert.AreEqual(typeof(Coffee), newCoffee.GetType());
+      }
+      
       [TestMethod]
       public void CoffeeCost_GetsCoffeeTotal_Int()
       {
